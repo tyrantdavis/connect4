373137fdb71ebbc26fe87f340a53ff323bc8a779
Tyrant Davis <davistyrant@gmail.com>
2018-09-11 12:05:41 -0500

    Build out Connect4 class, and #create_player, #play.

--- connect4.rb
+++ connect4.rb
@@ -1,13 +1,24 @@
-#connect4.rb
+require_relative 'player'
 
-def create_player()
+class Connect4
+  def create_player(player, name = STDIN)
+  	puts "Enter your name, or not, player #{player}?> "
+  	name = gets.chomp
+    name = "Player #{player}" if name.empty?
+    name
+  end
 
+  def initiate_game(name1, name2)
+    #TO DO
+  end
+
+  def play(input = STDIN, input2 = STDIN)
+    player = 1
+    player1 = create_player(player, input)
+    player2 = create_player(player += 1, input2)
+    game = initiate_game(player1, player2)
+    game.start
+  end
 end
-
-def initiate_game()
-
-end
-
-def play()
-
-end
+connect4 = Connect4.new
+connect4.play
